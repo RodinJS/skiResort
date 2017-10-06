7db1337564f0db6b66a5164c89b85542928700ab
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-10-06 11:51:25 +0400

    update gulp file, watch files changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+const batch = require('gulp-batch');
 const sequence = require('run-sequence');
 const del = require('del');
 const notify = require('gulp-notify');
@@ -10,48 +12,50 @@ const size = require('gulp-size');
 const connect = require('gulp-connect');
 const sourcemaps = require('gulp-sourcemaps');
 
-const JS = ['!systemjs/**/*.*','!node_modules/**/*.*','!dist/**/*.*','!build/**/*.*', '**/*.js'];
+const JS = ['!systemjs/**/*.*', '!node_modules/**/*.*', '!dist/**/*.*', '!build/**/*.*', '*.js', '**/*.js'];
 
 const ERROR_MESSAGE = {
-	errorHandler: notify.onError("Error: <%= error.message %>")
+    errorHandler: notify.onError("Error: <%= error.message %>")
 };
 
 gulp.task('js', () => {
-	const s = size({title: 'JS -> ', pretty: true});
-	return gulp.src(JS)
-		.pipe(plumber(ERROR_MESSAGE))
-		.pipe(babel())
-		.pipe(s)
-		.pipe(plumber.stop())
-		.pipe(gulp.dest('./build'))
-		.pipe(notify({
-			onLast: true,
-			message: () => `JS - Total size ${s.prettySize}`
-		}));
+    const s = size({title: 'JS -> ', pretty: true});
+    return gulp.src(JS)
+        .pipe(plumber(ERROR_MESSAGE))
+        .pipe(babel())
+        .pipe(s)
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('./build'))
+        .pipe(notify({
+            onLast: true,
+            message: () => `JS - Total size ${s.prettySize}`
+        }));
 });
 
 
-gulp.task('watch', () => {
-	gulp.watch(JS, ['js']);
+gulp.task('watch', function () {
+    watch(JS, batch(function (events, done) {
+        gulp.start(['dev'], done);
+    }));
 });
 
 gulp.task('clean', () => {
-	return del(['./build']);
+    return del(['./build']);
 });
 
 gulp.task('connect', () => {
-	connect.server({
-		root: './',
-		port: 9000,
-		livereload: true
-	});
+    connect.server({
+        root: './',
+        port: 9000,
+        livereload: true
+    });
 });
 
 gulp.task('dev', (done) => {
-	sequence('clean', 'js', done);
+    sequence('clean', 'js', done);
 });
 
 
 gulp.task('default', (done) => {
-	sequence('clean', ['js', 'connect'], done);
+    sequence('watch', ['js', 'connect'], done);
 });
